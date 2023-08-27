09f83fb831a61d9b09a7976c7bc01b1e9e6fffa2
djwnxo <kinjeok@gmail.com>
2023-08-27 11:18:27 +0900

    정종혁

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ random.seed(0)
 Entity.default_shader = lit_with_shadows_shader
 
 ground = Entity(model='plane', collider='box', scale=900, texture='grass', texture_scale=(4,4))
-ruined = Entity(model="ruined_portalobj", collider = "mesh", position=(0,0,0),scale = "30")
 
 editor_camera = EditorCamera(enabled=False, ignore_paused=True)
 player = FirstPersonController(model='cube', z=-10, color=color.orange, origin_y=-.5, speed=30, collider='box')
@@ -17,8 +16,10 @@ player.collider = BoxCollider(player, Vec3(0,1,0), Vec3(1,2,1))
 
 
 def pause_input(key):
-    if key == 'm':    # 미니맵
-        camera.position(player.position, player.position, player.position+10)
+    if key == 'tab':    # 시점 변환
+        editor_camera.enabled = not editor_camera.enabled
+        editor_camera.position = player.position
+
         
 
 pause_handler = Entity(ignore_paused=True, input=pause_input)
