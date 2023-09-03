3b0bcb3c0160bf113faa12c88b501aa4a31448c4
GamerChris0837 <jjangga4227@gmail.com>
2023-09-03 10:57:00 +0900

    dwd

--- Ursina-FPS.py
+++ Ursina-FPS.py
@@ -3,6 +3,7 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 from ursina.shaders import lit_with_shadows_shader
 
 app = Ursina()
+window.vsync = False
 random.seed(0)
 Entity.default_shader = lit_with_shadows_shader
 sun = DirectionalLight()
@@ -13,7 +14,7 @@ class FirstPersonController(Entity):
     def __init__(self, **kwargs):
         super().__init__()
         self.speed = 5
-        self.height = 2
+        self.height = 20
         self.camera_pivot = Entity(parent=self, y=self.height)
 
         camera.parent = self.camera_pivot
@@ -33,7 +34,6 @@ class FirstPersonController(Entity):
 
         self.traverse_target = scene
         self.ignore_list = [self, ]
-        self.on_destroy = self.on_disable
 
         for key, value in kwargs.items():
             setattr(self, key ,value)
@@ -91,54 +91,49 @@ class FirstPersonController(Entity):
         self.air_time = 0
         self.grounded = True
 
-if __name__ == '__main__':
-    from ursina.prefabs.first_person_controller import FirstPersonController
-    window.vsync = False
-    app = Ursina()
-    
-    ground = Entity(model='plane', scale=(100,1,100), texture='textures\doro.jpg', texture_scale=(1,1), collider='box')
-    apt = Entity(model='cube', scale=(5,100,10),position=(15,50,15), collider='box', texture='white_cube')
-    apt.texture_scale = (apt.scale_z, apt.scale_y)
-    apt = Entity(model='cube', scale=(1,5,10), x=-2, y=.01, collider='box', color=color.gray)
-    apt.texture_scale = (apt.scale_z, apt.scale_y)
-    apt = Entity(model='cube', scale=(5,100,10),position=(15,50,30), collider='box', texture='white_cube')
-    apt = Entity(model='cube', scale=(5,100,10),position=(30,50,30), collider='box', texture='white_cube')
-    player = FirstPersonController(y=2, origin_y=-.5)
-    player.gun = None
+ground = Entity(model='plane', scale=(100,1,100), texture='textures\doro.jpg', texture_scale=(1,1), collider='box')
+apt = Entity(model='cube', scale=(5,100,10),position=(15,50,15), collider='box', texture='white_cube')
+apt.texture_scale = (apt.scale_z, apt.scale_y)
+apt = Entity(model='cube', scale=(1,5,10), x=-2, y=.01, collider='box', color=color.gray)
+apt.texture_scale = (apt.scale_z, apt.scale_y)
+apt = Entity(model='cube', scale=(5,100,10),position=(15,50,30), collider='box', texture='white_cube')
+apt = Entity(model='cube', scale=(5,100,10),position=(30,50,30), collider='box', texture='white_cube')
+player = FirstPersonController(y=2, origin_y=-.5)
+player.gun = None
   
-    gun = Entity(model='assets\m4a1\M4A1.fbx', texture='assets\m4a1\mat0_c.jpg', parent=camera, position=(0.25,-0.15,0.5), scale=0.05, on_cooldown=False)
-    gullet = Entity(model='cube', parent=camera, scale=0.02, rotation_y=270, position=(0.25,-0.1,0.95), color=color.black, collision=True, visible=False)
-    suppressor = Entity(model='assets\Suppressor\source\low.obj', texture='assets\Suppressor\Textures\Suppressor_Base_color.png', parent=camera, scale=10)
+gun = Entity(model='assets\m4a1\M4A1.fbx', texture='assets\m4a1\mat0_c.jpg', parent=camera, position=(0.25,-0.15,0.5), scale=0.05, on_cooldown=False)
+gullet = Entity(model='cube', parent=camera, scale=0.02, rotation_y=270, position=(0.25,-0.1,0.95), color=color.black, collision=True, visible=False)
+suppressor = Entity(model='assets\Suppressor\source\low.obj', texture='assets\Suppressor\Textures\Suppressor_Base_color.png', parent=camera, scale=10)
 
-    shootables_parent = Entity()
-    mouse.traverse_target = shootables_parent  
+shootables_parent = Entity()
+mouse.traverse_target = shootables_parent  
 
-    bullet=None
-    def shoot():
-        global bullet
-        bullet = Entity(parent=gullet, model='cube', scale=(0.75,0.75,2), rotation_y=90, color=color.black, collision=True, collider="box")
-        bullet.world_parent = scene
-        bullet.animate_position(bullet.position+(bullet.forward*2500), curve=curve.linear, duration=1)
-        destroy(bullet, delay=1)
-        M4A1_gunfire.play()
-        Cartridge.play()
-        camera.shake(0.1,0.25)
+bullet=None
+def shoot():
+    global bullet
+    bullet = Entity(parent=gullet, model='cube', scale=(0.75,0.75,2), rotation_y=90, color=color.black, collision=True, collider="box")
+    bullet.world_parent = scene
+    bullet.animate_position(bullet.position+(bullet.forward*2500), curve=curve.linear, duration=1)
+    destroy(bullet, delay=1)
+    M4A1_gunfire.play()
+    Cartridge.play()
+    camera.shake(0.1,0.25)
 
-    M4A1_gunfire=Audio("assets\GunSounds\m4a1_gunshot.mp3", volume=0.3)
-    Cartridge=Audio("assets\GunSounds\Cartridge.mp3", volume=0.3)
+M4A1_gunfire=Audio("assets\GunSounds\m4a1_gunshot.mp3", volume=0.3)
+Cartridge=Audio("assets\GunSounds\Cartridge.mp3", volume=0.3)
     
-    def aim(key):
-        if held_keys['right mouse']:
-            gun.position=(0,-0.124,0.3)
-            gullet.position=(0,-0.124,1)
-            if held_keys['left mouse']:
-                shoot()
-        else:
-            gun.position=(0.25,-0.15,0.5)
-            gullet.position=(0.25,-0.1,0.95)
-            if held_keys['left mouse']:
-                shoot()
+def aim(key):
+    if held_keys['right mouse']:
+        gun.position=(0,-0.124,0.3)
+        gullet.position=(0,-0.124,1)
+        if held_keys['left mouse']:
+            shoot()
+    else:
+        gun.position=(0.25,-0.15,0.5)
+        gullet.position=(0.25,-0.1,0.95)
+        if held_keys['left mouse']:
+            shoot()
 
-    aim = Entity(input=aim)
+aim = Entity(input=aim)
 
-    app.run()
+app.run()
