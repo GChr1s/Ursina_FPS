8cb1bd086dbb79bb2a8145290cfacc49945c3220
djwnxo <kinjeok@gmail.com>
2023-09-03 11:58:04 +0900

    정종혁

--- Ursina-FPS.py
+++ Ursina-FPS.py
@@ -113,6 +113,12 @@ apt = Entity(model='cube', scale=(1,5,10), x=-2, y=.01, collider='box', color=co
 apt.texture_scale = (apt.scale_z, apt.scale_y)
 apt = Entity(model='cube', scale=(5,100,10),position=(15,50,30), collider='box', texture='white_cube')
 apt = Entity(model='cube', scale=(5,100,10),position=(30,50,30), collider='box', texture='white_cube')
+apt = Entity(model='cube', scale=(5,100,10),position=(30,50,15), collider='box', texture='white_cube')
+grass = Entity(model='assets\structure\source\Bush.fbx',scale=0.01,texture='assets\structure\Textures\grass3.png',position=(10,0,10))
+grass = Entity(model='assets\structure\source\Bush.fbx',scale=0.01,texture='assets\structure\Textures\grass3.png',position=(8,0,13))
+strl = Entity(model='assets\structure\source\Streetlights.fbx',scale=0.1,position=(25,0,6.5))
+for i in range(18,51):
+    grass = Entity(model='assets\structure\source\Bush.fbx',scale=0.01,texture='assets\structure\Textures\grass3.png',position=(i,0,6.5))
 player = FirstPersonController(y=2, origin_y=-.5)
 player.gun = None
   
