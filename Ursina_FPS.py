2dd286f5af85518add773e537062ab4362cc18e9
GamerChris0837 <jjangga4227@gmail.com>
2023-09-07 21:09:45 +0900

    dwd

--- Ursina_FPS.py
+++ Ursina_FPS.py
@@ -1,7 +1,7 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 from ursina.shaders import lit_with_shadows_shader
-
+from menu import menu
 
 app = Ursina()
 window.vsync = False
